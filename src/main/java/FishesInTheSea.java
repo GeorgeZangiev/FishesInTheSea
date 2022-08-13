9b7937192905a1eb612e94ce0ef718c57e3aa3bd
elmo <g.zangiev.2002@gmail.com>
2022-08-13 17:28:02 +0300

    dp deleted

--- src/main/java/FishesInTheSea.java
+++ src/main/java/FishesInTheSea.java
@@ -1,24 +1,18 @@
-import java.util.Arrays;
-
 public class FishesInTheSea {
 
-    private int collectFish(int[][] fish, int x, int y, int n, int m, int[][] dp)
+    private int collectFish(int[][] fish, int x, int y, int n, int m)
     {
         if ((x < 0) || (x == n) || (y == m)) {
             return 0;
         }
 
-        if (dp[x][y] != -1) {
-            return dp[x][y];
-        }
-
         int rightUpperDiagonal
-                = collectFish(fish, x - 1, y + 1, n, m, dp);
+                = collectFish(fish, x - 1, y + 1, n, m);
 
-        int right = collectFish(fish, x, y + 1, n, m, dp);
+        int right = collectFish(fish, x, y + 1, n, m);
 
         int rightLowerDiagonal
-                = collectFish(fish, x + 1, y + 1, n, m, dp);
+                = collectFish(fish, x + 1, y + 1, n, m);
 
         return fish[x][y] +
                 Math.max(Math.max(rightUpperDiagonal, rightLowerDiagonal), right);
@@ -27,13 +21,9 @@ public class FishesInTheSea {
     private int fishing(int[][] fish, int n, int m)
     {
         int maxFish = 0;
-        int[][] dp = new int[n][m];
-        for (int row = 0; row < n; row++) {
-            Arrays.fill(dp[row], -1);
-        }
         for (int i = 0; i < n; i++) {
             int fishCollected
-                    = collectFish(fish, i, 0, n, m, dp);
+                    = collectFish(fish, i, 0, n, m);
             maxFish = Math.max(maxFish, fishCollected);
         }
 
